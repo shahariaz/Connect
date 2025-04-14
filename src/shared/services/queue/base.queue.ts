719ad75148a73d3f85bc19208eeb157c4a701f32
shahariaz <shahariaz.info@gmail.com>
2025-04-14 14:07:10 +0600

    update baseQueue for addJob and ProcessJob

--- src/shared/services/queue/base.queue.ts
+++ src/shared/services/queue/base.queue.ts
@@ -4,34 +4,56 @@ import { BullAdapter } from "@bull-board/api/bullAdapter";
 import { ExpressAdapter } from "@bull-board/express";
 import { config } from "@root/config";
 import Logger from "bunyan";
+import { IAuthJob } from "../../../features/auth/interfaces/auth.interface";
 
-let bullAdapter: BullAdapter[] = [];
+type IBaseJobData = IAuthJob;
+
+let bullAdapters: BullAdapter[] = [];
 export let serverAdapter: ExpressAdapter;
 
 export abstract class BaseQueue {
   queue: Queue.Queue;
   log: Logger;
+
   constructor(queueName: string) {
     this.queue = new Queue(queueName, `${config.REDIS_HOST}`);
-    bullAdapter.push(new BullAdapter(this.queue));
-    bullAdapter = [...new Set(bullAdapter)];
+    bullAdapters.push(new BullAdapter(this.queue));
+    bullAdapters = [...new Set(bullAdapters)];
     serverAdapter = new ExpressAdapter();
     serverAdapter.setBasePath("/admin/queues");
 
     createBullBoard({
-      queues: bullAdapter,
+      queues: bullAdapters,
       serverAdapter,
     });
-    this.log = config.createLogger(`${queueName} Queue`);
+
+    this.log = config.createLogger(`${queueName}Queue`);
+
     this.queue.on("completed", (job: Job) => {
-      this.log.info(`Job ${job.id} completed!`);
       job.remove();
     });
+
     this.queue.on("global:completed", (jobId: string) => {
-      this.log.info(`Job ${jobId} completed!`);
+      this.log.info(`Job ${jobId} completed`);
     });
-    this.queue.on("stalled", (jobId: string) => {
-      this.log.info(`Job ${jobId} stalled!`);
+
+    this.queue.on("global:stalled", (jobId: string) => {
+      this.log.info(`Job ${jobId} is stalled`);
     });
+  }
+
+  protected addJob(name: string, data: IBaseJobData): void {
+    this.queue.add(name, data, {
+      attempts: 3,
+      backoff: { type: "fixed", delay: 5000 },
+    });
+  }
+
+  protected processJob(
+    name: string,
+    concurrency: number,
+    callback: Queue.ProcessCallbackFunction<void>
+  ): void {
+    this.queue.process(name, concurrency, callback);
   }
 }
