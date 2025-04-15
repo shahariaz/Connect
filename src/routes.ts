0acfaf34cbc07003732c861374672ec8b47e83b6
shahariaz <shahariaz.info@gmail.com>
2025-04-16 05:17:59 +0600

    setup routes for currentuser routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,14 @@ import { authRoutes } from "@auth/routes/authRoutes";
 import { serverAdapter } from "@service/queue/base.queue";
 import { Application } from "express";
 import { learnifyRouter } from "./features/learnify/route";
+import { currentUserRoutes } from "@auth/routes/currentRoutes";
 const BASE_PATH = "/api/v1";
 export default (app: Application) => {
   const routes = () => {
     app.use(BASE_PATH, authRoutes.routes());
     app.use(BASE_PATH, learnifyRouter.routes());
     app.use(BASE_PATH, authRoutes.signoutRoute());
+    app.use(BASE_PATH, currentUserRoutes.routes());
     // Only add the queue route if serverAdapter exists
     if (serverAdapter && typeof serverAdapter.getRouter === "function") {
       app.use("/admin/queues", serverAdapter.getRouter());
