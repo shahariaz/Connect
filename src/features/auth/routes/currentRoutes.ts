63f0115d982dcc8ba06636b143a5effcfde8ba85
shahariaz <shahariaz.info@gmail.com>
2025-04-21 15:56:15 +0600

    feat: setup auth middleware in route

--- src/features/auth/routes/currentRoutes.ts
+++ src/features/auth/routes/currentRoutes.ts
@@ -1,4 +1,5 @@
 import { CurrentUser } from "@auth/controllers/current-user";
+import { authMiddleware } from "@global/helpers/auth-middleware";
 import express, { Router } from "express";
 class CurrentUserRoutes {
   private router: Router;
@@ -6,7 +7,11 @@ class CurrentUserRoutes {
     this.router = express.Router();
   }
   public routes(): Router {
-    this.router.get("/currentuser", CurrentUser.prototype.read);
+    this.router.get(
+      "/currentuser",
+      authMiddleware.checkAuthenticated,
+      CurrentUser.prototype.read
+    );
     return this.router;
   }
 }
